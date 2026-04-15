29b36255144594bfcc3c7a54ff15f9df2b63de0d
prasad <prasad2082001@gmail.com>
2026-04-15 16:04:50 +0530

    Added UC7 generic target unit addition

---
+++ src/main/java/org/example/Length.java
@@ -0,0 +1,102 @@
+package org.example;
+
+public class Length {
+    private double value;
+    private LengthUnit unit;
+
+    public Length(double value, LengthUnit unit){
+        if(Double.isNaN(value) || Double.isInfinite(value)) throw new IllegalArgumentException("Invalid numeric value");
+        if(unit == null) throw new IllegalArgumentException("This exception is thrown because Unit is provided as null");
+        this.value = value;
+        this.unit = unit;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public LengthUnit getUnit() {
+        return unit;
+    }
+
+    private double convertToBaseUnit(){
+        return Math.round(value*unit.getConversionFactor() *1000.0)/1000.0;
+    }
+
+
+    public Length convertTo(LengthUnit targetUnit){
+        if(targetUnit == null) throw new IllegalArgumentException(" Target unit cannot be null");
+
+        double valueOfBaseUnit = this.value * this.unit.getConversionFactor();
+        double convertedValue = Math.round(valueOfBaseUnit / targetUnit.getConversionFactor() *1000.0)/1000.0;
+        return new Length(convertedValue, targetUnit);
+    }
+
+    public Length add(Length secondLength){
+        if(secondLength == null) throw new IllegalArgumentException("Length to add cannot be null cannot be null");
+        double firstValue = this.value * this.unit.getConversionFactor();
+        double secondValue = secondLength.value * secondLength.unit.getConversionFactor();
+        double sumValue = firstValue + secondValue;
+        double resultValue = Math.round(sumValue / this.unit.getConversionFactor() *1000.0)/1000.0;
+        return new Length(resultValue, this.unit);
+    }
+
+    public Length add(Length secondlength, LengthUnit targetUnit){
+        if(targetUnit == null) throw new IllegalArgumentException("Please, Enter a valid targetUnit");
+        return addAndConvert(secondlength, targetUnit);
+    }
+
+    private Length addAndConvert(Length secondlength, LengthUnit targetUnit) {
+        double base1 = this.value * this.unit.getConversionFactor();
+        double base2 = secondlength.value * secondlength.unit.getConversionFactor();
+        double baseValueInInches = base1 + base2;
+
+        double resultValue = Math.round(convertFromBaseToTargetUnit(baseValueInInches, targetUnit) *1000.0)/1000.0;
+        return new Length(resultValue, targetUnit);
+    }
+
+    private double convertFromBaseToTargetUnit(double baseValueInInches, LengthUnit targetUnit) {
+        return baseValueInInches / targetUnit.getConversionFactor();
+    }
+
+    @Override
+    public String toString() {
+        return ""+value+" "+unit;
+    }
+
+    public boolean compare(Length thatLength){
+        return Double.compare(
+                this.convertToBaseUnit(),
+                thatLength.convertToBaseUnit()
+        ) == 0;
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if(this == obj) return true;
+        if(obj == null || getClass() != obj.getClass()) return false;
+        Length length = (Length)obj;
+        return compare(length);
+    }
+
+    @Override
+    public int hashCode(){
+        return Double.hashCode(convertToBaseUnit());
+    }
+
+    public enum LengthUnit{
+        FEET(12.0),
+        INCHES(1.0),
+        YARDS(36.0),
+        CENTIMETERS(0.393701);
+
+        private final double conversionFactor;
+
+        LengthUnit(double conversionFactor){
+            this.conversionFactor = conversionFactor;
+        }
+        public double getConversionFactor(){
+            return conversionFactor;
+        }
+    }
+}
